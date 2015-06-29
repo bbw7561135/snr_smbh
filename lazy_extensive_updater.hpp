e33ef9dea8395efcaaf2bfc68adce92735cbd716
Almog Yalinewich <almog.yalin@gmail.com>
2015-06-29 06:31:41 +0300

    extract lazy extensive updater

---
+++ lazy_extensive_updater.hpp
@@ -0,0 +1,26 @@
+#ifndef LAZY_EXTENSIVE_UPDATER_HPP
+#define LAZY_EXTENSIVE_UPDATER_HPP 1
+
+#include "source/newtonian/two_dimensional/extensive_updater.hpp"
+
+class LazyExtensiveUpdater: public ExtensiveUpdater
+{
+public:
+
+  LazyExtensiveUpdater(const Tessellation& tess,
+		       const PhysicalGeometry& pg);
+
+  void operator()
+  (const vector<Extensive>& fluxes,
+   const PhysicalGeometry& /*pg*/,
+   const Tessellation& tess,
+   const double dt,
+   const CacheData& cd,
+   const vector<ComputationalCell>& cells,
+   vector<Extensive>& extensives) const;
+
+private:
+  const vector<double> lengths_;
+};
+
+#endif // LAZY_EXTENSIVE_UPDATER_HPP
--- rich.cpp
+++ rich.cpp
@@ -55,60 +55,10 @@
 #include "log_cfl.hpp"
 #include "entropy_fix.hpp"
 #include "edge_length_calculator.hpp"
+#include "lazy_extensive_updater.hpp"
 
 using namespace std;
 using namespace simulation2d;
-
-namespace {
-
-  bool bracketed(double low, double arg, double high)
-  {
-    return arg>=low and high>arg;
-  }
-
-  class LazyExtensiveUpdater: public ExtensiveUpdater
-  {
-  public:
-
-    LazyExtensiveUpdater(const Tessellation& tess,
-			 const PhysicalGeometry& pg):
-      lengths_(serial_generate(EdgeLengthCalculator(tess,pg))) {}
-
-    void operator()
-    (const vector<Extensive>& fluxes,
-     const PhysicalGeometry& /*pg*/,
-     const Tessellation& tess,
-     const double dt,
-     const CacheData& cd,
-     const vector<ComputationalCell>& cells,
-     vector<Extensive>& extensives) const
-    {
-      const vector<Edge>& edge_list = tess.getAllEdges();
-      for(size_t i=0;i<cells.size();++i){
-	extensives[i].tracers["entropy"] = 
-	  cd.volumes[i]*cells[i].density*
-	  cells[i].tracers.find("entropy")->second;
-      }
-      for(size_t i=0;i<edge_list.size();++i){
-	const Edge& edge = edge_list[i];
-	const Extensive delta = dt*lengths_[i]*fluxes[i];
-	if(bracketed(0,edge.neighbors.first,tess.GetPointNo())){
-	  extensives[static_cast<size_t>(edge.neighbors.first)] -=
-	    delta;
-	  assert(extensives[static_cast<size_t>(edge.neighbors.first)].tracers.find("entropy")->second>0);
-	}
-	if(bracketed(0,edge.neighbors.second,tess.GetPointNo())){
-	  extensives[static_cast<size_t>(edge.neighbors.second)] +=
-	    delta;
-	  assert(extensives[static_cast<size_t>(edge.neighbors.second)].tracers.find("entropy")->second>0);
-	}
-      }
-    }
-
-  private:
-    const vector<double> lengths_;
-  };
-}
 
 class SimData
 {
