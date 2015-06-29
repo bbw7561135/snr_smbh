49ac1f82a0ed7ddcd3730238eb4e08c45dc504e8
Almog Yalinewich <almog.yalin@gmail.com>
2015-06-29 07:37:22 +0300

    simplify main loop

--- my_main_loop.cpp
+++ my_main_loop.cpp
@@ -6,27 +6,20 @@
 #include "source/newtonian/test_2d/multiple_diagnostics.hpp"
 #include "source/tessellation/shape_2d.hpp"
 #include "supernova.hpp"
+#include "source/misc/vector_initialiser.hpp"
 
 using namespace simulation2d;
 
 void my_main_loop(hdsim& sim, const Constants& c)
 {
-  const double tf = 1e3*c.year;
+  const double tf = 1e2*c.year;
   SafeTimeTermination term_cond_raw(tf,1e6);
-  ConsecutiveSnapshots diag1(new ConstantTimeInterval(tf/10),
-			     new Rubric("snapshot_",".h5"));
-  WriteTime diag2("time.txt");
-  WriteConserved diag4("conserved.txt");
-  //Bremsstrahlung diag5("luminosity_history.txt",c);
-  WriteCycle diag6("cycle.txt");
-  vector<DiagnosticFunction*> diag_list;
-  diag_list.push_back(&diag1);
-  diag_list.push_back(&diag2);
-  //    diag_list.push_back(&diag3);
-  diag_list.push_back(&diag4);
-  //    diag_list.push_back(&diag5);
-  diag_list.push_back(&diag6);
-  MultipleDiagnostics diag(diag_list);
+  MultipleDiagnostics diag
+    (VectorInitialiser<DiagnosticFunction*>()
+     (new ConsecutiveSnapshots(new ConstantTimeInterval(tf/10),
+			       new Rubric("snapshot_",".h5")))
+     (new WriteTime("time.txt"))
+     (new WriteCycle("cycle.txt"))());
   const Circle hot_spot(Vector2D(0,-c.offset),
 			c.supernova_radius);
   Supernova manip(hot_spot,
